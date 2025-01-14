4c8b229fe02db05db6c6de4457ed3b4b9df704ce
Amit55Mandal <AMITMAN6565@GMAIL.COM>
2025-01-14 20:08:34 +0530

    Delete src/Privatecomp.js

--- src/Privatecomp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-const PrivateComponent=()=>{
-    const auth=localStorage.getItem('user');
-    return auth?<Outlet></Outlet>:<Navigate to="/singup"></Navigate>
-}
-export default PrivateComponent;
